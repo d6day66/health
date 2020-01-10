d70b6b3ad072bd341fc89236b1913249da7d1192
d6day66 <d6day@126.com>
2020-01-10 22:16:49 +0800

    2020-01-10create health project

---
+++ health_common/src/main/java/com/itheima/constant/RedisConstant.java
@@ -0,0 +1,6 @@
+package com.itheima.constant;
+
+public class RedisConstant {
+    public static final String SETMEAL_PIC_RESOURCES = "setmealPicResources";
+    public static final String SETMEAL_DB_RESOURCES = "setmealPicDbResources";
+}
